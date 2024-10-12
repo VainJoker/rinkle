8c286c060c2622e921278656e7638068a1ae9bba
Hanjie Zhang <vainjoker@tuta.io>
2024-10-13 05:28:12 +0800

    ✨ feat: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use rinkle::prelude::*;
+
+#[allow(clippy::unnecessary_wraps)]
+fn main() -> Result<()> {
+	println!("Hello, World!");
+	Ok(())
+}
---
+++ tests/healthy_test.rs
@@ -0,0 +1,4 @@
+#[test]
+fn healthy() {
+	assert_eq!(2 + 2, 4);
+}
